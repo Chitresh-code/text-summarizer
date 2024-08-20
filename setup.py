a7f1f51ffd2fbaab3fb08b3840333218964ce083
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-08-20 21:05:29 +0530

    fail

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
-    
+
+
 __version__ = "0.0.0"
 
-REPO_NAME = "text-summarizer"
-AUTHOR_USER_NAME = "Chitresh"
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "gychitresh1290@gmail.com"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
+
+
 
 setuptools.setup(
     name=SRC_REPO,
--- template.py
+++ template.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level = logging.INFO, format = '[%(asctime)s: %(message)s]')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 project_name = "textSummarizer"
 
@@ -29,18 +30,23 @@ list_of_files = [
 
 ]
 
+
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    
+
     if filedir != "":
-        os.makedirs(filedir, exist_ok = True)
-        logging.info(f"Creating directory: {filedir} for the file {filename}")
-        
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, 'w') as f:
+        with open(filepath,'w') as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
+
     
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
+
